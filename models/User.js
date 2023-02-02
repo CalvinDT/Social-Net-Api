de881479709bac1f9db5210c446a76c27827d3e7
CalvinDT <ct223344@gmail.com>
2023-02-01 20:44:44 -0500

    creates models and controllers

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+// users schema
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// creates User model using UserSchema
+const User = model('User', UserSchema);
+
+// gets total count of all friends
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+module.exports = User;
